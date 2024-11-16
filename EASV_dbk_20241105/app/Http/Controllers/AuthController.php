0fa7c9855f38482005f637d365835bcb59a98f21
OnczUwU <140672241+OnczUwU@users.noreply.github.com>
2024-11-16 04:24:46 +0100

    Update AuthController.php

--- EASV_dbk_20241105/app/Http/Controllers/AuthController.php
+++ EASV_dbk_20241105/app/Http/Controllers/AuthController.php
@@ -2,69 +2,83 @@
 
 namespace App\Http\Controllers;
 
-use Hash;
-use Illuminate\Http\JsonResponse;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     //
-    public function register(Request $request): void{
-        $validatedData = $request ->validate(rules: [
-            'name'=>['required','string','max:225'],
-            'email'=>['required','string','max:225','unique:users'],
+ public function register(Request $request){
+        $validatedData = $request->validate([
+            'name'=>['required','string','max:255'],
+            'email'=>['required','string','email','max:255','unique:users'],
             'password'=>['required','string','min:8','max:20'],
         ]);
 
-        $User = User::create([
+        $user = User::create([
             'name'=> $validatedData['name'],
             'email'=> $validatedData['email'],
             'password'=> Hash::make($validatedData['password']),
         ]);
 
-        $token = $User->createToken('auth_token')->plainTextToken;
+        $token = $user->createToken('auth_token')->plainTextToken;
 
-        return response()-> json([
-            "Success"=>true,
+        return response()->json([
+            "success"=> true,
             "errors"=>[
                 "code"=>0,
                 "msg"=>""
             ],
             "data"=>[
                 "access_token"=>$token,
-                "token_type" =>"Beaber"
+                "token_type" => "Bearer"
             ],
-            "msg"=>"Usuario creado sastisfactoriamente",
+            "msg"=>"Usuario creado satisfactoriamente",
             "count"=>1
         ]);
-
-      
-        
     }
-    public function logout(Request $request){
-        if(!Auth::attempt($request->only("email","password"))){
-            "success"=> false,
-            "errors"=>401,
-            "msg"=> "No se reconocen las credenciales"
-        
-    }
+}
 
-
-    public function me(Request $request){
-        return response()->json([
+public function login(Request $request) {
+        if(!Auth::attempt($request->only( "email","password"))){
+            return response()->json( [
+                "success"=> false,
+                "errors"=>[
+                    "code"=>401,
+                    "msg"=> "No se reconocen las credenciales"
+                ],
+                "data"=>"",
+                "count"=> 0
+            ], 401);
+        }
+        $user = User::where("email", $request->email)->firstOrFail();
+        $token = $user->createToken("auth_token")->plainTextToken;
+        return response()->json( [
             "success"=> true,
             "errors"=>[
-                "code"=> 2000,
+                "code"=>200,
                 "msg"=> ""
-                
-                ],
-                "data"=>"Ha iniciado sesion correctamente",
-                "count"=> 1
-                ],status:200);
+            ],
+            "data"=>[
+                "access_token"=>$token,
+                "token_type"=> "Bearer"
+            ],
+            "count"=> 1
+        ], 200);
+    
+    
     }
-
-
-
-
+    public function me(Request $request) {
+        return response()->json( [
+            "success"=> true,
+            "errors"=>[
+                "code"=>200,
+                "msg"=> ""
+            ],
+            "data"=>$request->user(),
+            "count"=> 1
+        ], 200);
+    }
 }
